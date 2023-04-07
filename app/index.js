4106eb4b60f6098387554145c0a4498dca66b41b
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-04-07 22:09:36 +0530

    initial commit

--- app/index.js
+++ app/index.js
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack , useRouter } from 'expo-router';
+
+import { COLORS, Colors, icons, images, SIZES } from '../constants';
+
+import {
+    Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome
+} from '../components';
+
 
 const Home = () => {
+
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>
-                Home 
-            </Text>
-        </View>
+      <SafeAreaView style= {{ flex: 1, backgroundColor : COLORS. lightWhite}} >
+         <Stack.Screen
+           options={{
+            headerStyle: { backgroundColor:COLORS.lightWhite},
+            headerShadowVisible: false,
+            headerLeft: () => (
+                <ScreenHeaderBtn iconUrl= {icons.menu} dimensions="60%" />
+            ),
+            headerRight: () => (
+                <ScreenHeaderBtn iconUrl={icons.profile} dimensions="100%" />
+            ),
+            headerTitle: ""
+
+           }} 
+           
+           />
+
+           
+
+      </SafeAreaView>
     )
 }
 
