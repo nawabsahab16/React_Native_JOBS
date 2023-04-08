2baf1b993bec24bb7567ad14405b499d62f97a64
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-04-08 16:33:13 +0530

    initial commit

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native'
 
+import { useRouter } from 'expo-router';
 import styles from './welcome.style'
+import { icons, SIZES} from '../../../constants';
+
+
 
 const Welcome = () => {
+   const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.userName}> Hello Sameer !</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect Job</Text>
+    </View> 
+
+     <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+           <TextInput 
+              style={styles.searchInput}
+           />
+        </View>
+     </View>
+
+
+
+   </View>
   )
 }
 
